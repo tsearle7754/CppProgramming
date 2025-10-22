917a1de4eec952868d98d0dee4d5c6c26a090460
tambryn.s <tsearle7754@floridapoly.edu>
2025-10-22 09:48:43 -0400

    oct 22 class

---
+++ oct_22.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+// LOOK OVER RATIONAL CLASS- chp 14
+
+// Friend functions and classes
+// allows you to declare friend functions and classes for class so these func/classes can access the class's private members.
+
+// If name of variable and parameter are the same, must use this-> for the variable (date friend function code)
+// friend class- will be in header file
