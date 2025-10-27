e49cbfa9f714e8cbbe09c479feacdc98843d7a0b
tambryn.s <tsearle7754@floridapoly.edu>
2025-10-27 17:29:07 -0400

    practice

--- Practice/practice5.h
+++ Practice/practice5.h
@@ -9,21 +9,22 @@ private:
 public:
     // constructor
     Complex();
-    Complex(double real, double img);
+    Complex(double r, double i);
 
     // getters
     double getReal() const;
     double getImg() const;
 
     // setters
-    void setReal(double r) const;
-    void setImg(double i) const;
+    void setReal(double r);
+    void setImg(double i);
 
     // methods
     void printComplex();
     Complex addition(Complex &comp);
 };
 
+// outside of class
 Complex additionTwo(Complex &comp1, Complex &comp2);
 Complex additionArray(Complex complexArray[], int size);
 
