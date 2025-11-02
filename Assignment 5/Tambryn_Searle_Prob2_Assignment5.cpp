882ff17292106adc5513a4f2d1340e7680dcae70
tambryn.s <tsearle7754@floridapoly.edu>
2025-11-02 13:27:17 -0500

    Assignment 5 complete

--- Assignment 5/Tambryn_Searle_Prob2_Assignment5.cpp
+++ Assignment 5/Tambryn_Searle_Prob2_Assignment5.cpp
@@ -18,3 +18,19 @@ bool isGreater(const T list1[], const T list2[], int size1, int size2) {
     }
     return min2 > max1;
 }
+
+int main() {
+    int a1[] = {1, 3, -3, 80};
+    int a2[] = {81, 90, 50};
+    cout << "Int arrays: " << (isGreater(a1, a2, 4, 3) ? "true" : "false") << endl;
+
+    double d1[] = {1.57, 3.14159, -360, 0};
+    double d2[] = {5.5, 10.1};
+    cout << "Double arrays: " << (isGreater(d1, d2, 4, 2) ? "true" : "false") << endl;
+
+    string s1[] = {"abcd", "ab", "Adad", "Cat"};
+    string s2[] = {"dog", "zebra"};
+    cout << "String arrays: " << (isGreater(s1, s2, 4, 2) ? "true" : "false") << endl;
+
+    return 0;
+}
