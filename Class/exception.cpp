3873cf88934dca420134dc38aeee2acf720c7d48
tambryn.s <tsearle7754@floridapoly.edu>
2025-11-10 19:05:08 -0500

    nov 10

---
+++ Class/exception.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+// exceptions will lead to successful terminations of the program.
+// if loops- low level exception
+// exit(); -- terminates program successfully/
--- Class/polymorphism.cpp
+++ Class/polymorphism.cpp
@@ -11,6 +11,13 @@ using namespace std;
 // use WithVirtualtoString  -- will make function virtual
 // virtual: if base class is virtual, all other functions will be virtual (no need to redefine)
 // Virtual Functions: invoking a function from child class that is also defined in parent class
+
 // Dynamic Binding: 
 //  --function must be defined virtual in the base class
 //  --variable that references the object for the func must contain the address of thr object
+
+// Abstract functions- declare getter/setter in header file of parent class, implement in child class
+    // -- parent class becomes an abstract class
+// pass parent class- will pass child classes as well -- (const GeometricObject& g)
+
+// start writing from the main function- pseudocode
